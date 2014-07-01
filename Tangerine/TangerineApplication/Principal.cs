015e6ad1c2b86a2cb5a666e4b629daf0d6fcbfed
Carlos Astorga <mi.emilio@outlook.com>
2014-06-30 20:56:16 -0600

    Revert "Revert "30-06-2014""
    This reverts commit 806cbd9bda76fc7b5050c3db4511b526baba4417.

--- Tangerine/TangerineApplication/Principal.cs
+++ Tangerine/TangerineApplication/Principal.cs
@@ -56,7 +56,7 @@ namespace TangerineApplication
             Boolean encontrado = false;
             while (i < hijos.Length && !encontrado)
             {
-                if (hijos[i].Name == "EliminarCliente")
+                if (hijos[i].Name == "NuevoCliente")
                 {
                     encontrado = true;
                     hijos[i].Focus();
@@ -66,9 +66,9 @@ namespace TangerineApplication
             }//buscar
             if (!encontrado)
             {
-                EliminarCliente eliminarCliente = new EliminarCliente();
-                eliminarCliente.MdiParent = this;
-                eliminarCliente.Show();
+                NuevoCliente nuevoCliente = new NuevoCliente();
+                nuevoCliente.MdiParent = this;
+                nuevoCliente.Show();
             }
 
         }//Nuevo Cliente
